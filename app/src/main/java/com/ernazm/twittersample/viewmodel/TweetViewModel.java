de991cf30ece7a63243ae02eaa59541c308da68c
ernazm <ernazm@gmail.com>
2017-02-11 23:59:19 +0200

    minor adjustments

--- app/src/main/java/com/ernazm/twittersample/viewmodel/TweetViewModel.java
+++ app/src/main/java/com/ernazm/twittersample/viewmodel/TweetViewModel.java
@@ -1,26 +1,18 @@
 package com.ernazm.twittersample.viewmodel;
 
 import android.databinding.ObservableField;
-import android.os.Build;
 import android.text.Html;
-import android.text.Spanned;
-import android.text.SpannedString;
 
 import com.ernazm.twittersample.datamodel.Tweet;
 
 public class TweetViewModel {
-    public final ObservableField<String> name = new ObservableField<>();
-    public final ObservableField<Spanned> content = new ObservableField<>();
-    public final ObservableField<String> site = new ObservableField<>();
+    public final ObservableField<String> username = new ObservableField<>();
+    public final ObservableField<CharSequence> content = new ObservableField<>();
+    public final ObservableField<CharSequence> source = new ObservableField<>();
 
     public TweetViewModel(Tweet tweet) {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//            content.set(Html.fromHtml(tweet.getElementPureHtml(), Html.FROM_HTML_MODE_LEGACY));
-//        } else {
-//            content.set(Html.fromHtml(tweet.getElementPureHtml()));
-//        }
-        content.set(new SpannedString(tweet.getText()));
-        name.set(tweet.getUser().getName());
-        site.set(tweet.getSource());
+        username.set(tweet.getUser().getName());
+        content.set(Html.fromHtml(tweet.getText()));
+        source.set(Html.fromHtml(tweet.getSource()));
     }
 }
